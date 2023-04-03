ac2276f687bc4acec5e4c5c3e20251619e134226
DjurovicAleksandar <djur.aleksandar@gmail.com>
2023-04-03 12:16:48 +0200

    Fixing contaact support page

--- src/components/administrator/options/ContactAdmin.jsx
+++ src/components/administrator/options/ContactAdmin.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function ContactAdmin() {
   const options = [
-    ["Poruka", "sms:+38766151382"],
+    // ["Poruka", "sms:+38766151382"],
     ["Poziv", "tel:+38766151382"],
     ["Mail", "mailto:@ekonobar1@gmail.com"],
   ];
@@ -18,20 +18,24 @@ function ContactAdmin() {
             {options.map(([title, path], index) => {
               return (
                 <li
+                  onClick={(e) => {
+                    window.open(path);
+                    console.log(e.target);
+                  }}
                   key={index}
                   className="w-full h-[4rem] bg-white text-[#212121] py-[0.8rem] mb-[1.2rem] rounded-md text-[1.5rem] hover:scale-110 active:scale-90 ease-in-out duration-300"
                 >
-                  <a href={path}>{title}</a>
+                  {title}
                 </li>
               );
             })}
           </ul>
-          <div className="flex items-center justify-around mt-72">
-            <Link to="/administrator-page">
+          <div className="flex items-center justify-around mt-[25rem]">
+            {/* <Link to="/administrator-page">
               <button className="rounded-md bg-transparent text-center py-[1rem] text-white border-[0.1rem] border-white ease-in-out duration-300 active:scale-90 cursor-pointer w-[12rem] ">
                 Nazad
               </button>
-            </Link>
+            </Link> */}
           </div>
           <div className="text-center text-[0.7rem] w-full p-4 ">
             {" "}
